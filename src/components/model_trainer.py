3ac48eae85580bb87c37a223d2eb5c22bd352260
Oshobugie <oskanabe55@gmail.com>
2026-09-17 10:46:36 +0000

    hyperparameter training

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -55,16 +55,36 @@ class ModelTrainer:
             }
 
             params = {
+                "Decision Tree": {
+                    'criterion': ['squared_error', 'absolute_error', 'poisson'],
+                },
+                "Random Forest Regressor":{
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
                 "Linear Regression": {},
                 "Lasso": {},
                 "Ridge": {},
-                "K-Neighbors Regressor": {},
-                "Decision Tree": {},
-                "Random Forest Regressor": {},
-                "XGBRegressor": {}, 
-                "CatBoosting Regressor": {},
-                "AdaBoost Regressor": {},
-                "Gradient Boosting": {} # <-- FIXED: Added this so it matches the models dict!
+                "K-Neighbors Regressor": {
+                    'n_neighbors': [5, 7, 9, 11]
+                },
+                "XGBRegressor": {
+                    'learning_rate': [.1, .01, .05, .001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "CatBoosting Regressor": {
+                    'depth': [6, 8, 10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor": {
+                    'learning_rate': [.1, .01, 0.5, .001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "Gradient Boosting": {
+                    'learning_rate': [.1, .01, .05, .001],
+                    'subsample': [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                }
             }
 
             model_report: dict = evaluate_models(X_train=x_train, y_train=y_train, X_test=x_test, y_test=y_test, models=models, param=params)
